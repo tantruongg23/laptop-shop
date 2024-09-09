e81e67af75695666dc9ecb35543d1a248e83900c
Tan Truong <tanfctk@gmail.com>
2024-09-09 22:57:27 +0700

    hello world

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -0,0 +1,15 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public String helloWorld() {
+    return "Hello World from Spring Boot";
+  }
+
+}
